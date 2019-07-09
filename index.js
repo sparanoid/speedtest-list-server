518405d53312a7980f00b1e472f223e264956483
Tunghsiao Liu <t@sparanoid.com>
2019-07-09 11:11:15 +0800

    feat: update upload method

--- index.js
+++ index.js
@@ -13,68 +13,72 @@ const client = algoliasearch(
 const index = client.initIndex(process.env.ALGOLIA_INDEX);
 const url = 'https://c.speedtest.net/speedtest-servers-static.php';
 const filename = 'speedtest-list.json';
+const processData = (data) => {
+  parser.parseString(data, function(err, content) {
+    if (err) throw err;
+
+    // Remove top-level "$" of every server node
+    let result = content.settings.servers[0].server.map(item => {
+      return item.$;
+    })
+
+    // Convert strings to number
+    for (let i = 0; i < result.length; i++) {
+      var obj = result[i];
+      for (let prop in obj) {
+        // Use reversed objectID
+        // https://www.algolia.com/doc/api-client/methods/indexing/#index-objects
+        obj.objectID = obj.id;
+
+        // Use reversed geolocation
+        // https://www.algolia.com/doc/guides/managing-results/refine-results/geolocation/
+        obj._geoloc = {};
+        obj._geoloc.lat = +obj.lat;
+        obj._geoloc.lon = +obj.lon;
+      }
+    }
+
+    // Remove duplicated props
+    for (let i = 0; i < result.length; i++) {
+      var obj = result[i];
+      for (let prop in obj) {
+        delete obj.id;
+        delete obj.lat;
+        delete obj.lon;
+      }
+    }
+
+    let output = JSON.stringify(result, null, 2)
+
+    fs.writeFile(filename, output, function(err) {
+      if (err) throw err;
+      console.log(`List saved to ${filename}`);
+    });
+
+    index.setSettings({
+      attributesForFaceting: [
+        'country'
+      ],
+      maxValuesPerFacet: 1000
+    });
+
+    // Clear index first to avoid "Record quota exceeded" error for free plan
+    index.clearIndex((err, content) => {
+      if (err) throw err;
+      console.log(`Index cleared`);
+    });
+
+    index.saveObjects(result, (err, content) => {
+      if (err) throw err;
+      console.log(`List uploaded to Algolia`);
+    });
+  });
+}
 
 axios.get(url)
   .then(function (response) {
     let xml = response.data;
-
-    parser.parseString(xml, function(err, content) {
-      if (err) throw err;
-
-      // Remove top-level "$" of every server node
-      let result = content.settings.servers[0].server.map(item => {
-        return item.$;
-      })
-
-      // Convert strings to number
-      for (let i = 0; i < result.length; i++) {
-        var obj = result[i];
-        for (let prop in obj) {
-          // Use reversed objectID
-          // https://www.algolia.com/doc/api-client/methods/indexing/#index-objects
-          obj.objectID = obj.id;
-
-          // Use reversed geolocation
-          // https://www.algolia.com/doc/guides/managing-results/refine-results/geolocation/
-          obj._geoloc = {};
-          obj._geoloc.lat = obj.lat;
-          obj._geoloc.lon = obj.lon;
-
-          if (obj.hasOwnProperty(prop) && obj[prop] !== null && !isNaN(obj[prop])){
-            obj[prop] = +obj[prop];
-          }
-        }
-      }
-
-      // Remove duplicated props
-      for (let i = 0; i < result.length; i++) {
-        var obj = result[i];
-        for (let prop in obj) {
-          delete obj.id;
-          delete obj.lat;
-          delete obj.lon;
-        }
-      }
-
-      let output = JSON.stringify(result, null, 2)
-
-      fs.writeFile(filename, output, function(err) {
-        if (err) throw err;
-        console.log(`List saved to ${filename}`);
-      });
-
-      index.setSettings({
-        attributesForFaceting: [
-          'country'
-        ],
-        maxValuesPerFacet: 1000
-      });
-
-      index.addObjects(result, (err, content) => {
-        if (err) throw err;
-        console.log(`List uploaded to Algolia`);
-      });
-    });
+    processData(xml);
   })
   .catch(function (error) {
     console.log(error);
